f084c11fe33b7fd9069e2621634c00898b1eefd8
eslam shosha <you@example.comeslamshosha22.es@gmail.com>
2024-05-28 03:52:42 +0300

    first

---
+++ js/script.js
@@ -0,0 +1,113 @@
+let hint = document.querySelector(".preloader");
+window.onload = function () {
+  //hide the preloader
+  setTimeout(function () {
+    hint.style.display = "none";
+  }, 700);
+};
+$(document).ready(function () {
+  if ($(window).width() >= 991) {
+    sal({
+      once: true,
+    });
+  } else {
+    sal({
+      disabled: true,
+    });
+  }
+
+  //phone size menu onclick
+  $("#menu-id").click(function (e) {
+    e.preventDefault();
+    $(".navgition").toggleClass("reset-left");
+    $("body").toggleClass("overflow");
+  });
+  $(".nav-head .close-menu").click(function () {
+    $(".navgition").removeClass("reset-left");
+    $("body").removeClass("overflow");
+  });
+  if ($(window).width() <= 1199) {
+    //slide down menu
+    $(".lang-anchor .lang-cont").click(function (e) {
+      e.preventDefault();
+      $(this).siblings(".dropdown-content").slideToggle(400);
+    });
+    $(".close-menu").click(function (e) {
+      e.preventDefault();
+      $(".lang-anchor .lang-cont").siblings(".dropdown-content").slideUp(400);
+    });
+  }
+  //////////** fixed arrow to top**//////////
+  $(".arrow-top").click(function () {
+    $("html").css("scroll-behavior", "unset");
+
+    $("html,body").animate(
+      {
+        scrollTop: 0,
+      },
+      1000,
+      "swing"
+    );
+    setTimeout(() => {
+      $("html").css("scroll-behavior", "smooth");
+    }, 1000);
+  });
+  $(this).scrollTop() >= 500
+    ? $(".arrow-top").fadeIn(300)
+    : $(".arrow-top").fadeOut(300);
+
+  $(window).scroll(function () {
+    $(this).scrollTop() >= 500
+      ? $(".arrow-top").fadeIn(300)
+      : $(".arrow-top").fadeOut(300);
+  });
+  const selectExists =
+    document.getElementsByClassName("select_input").length > 0;
+  if (selectExists) {
+    const $select2 = $(".select_input");
+    $select2.select2();
+  }
+
+  ///////// ** championship-slider** /////////
+  var specialsO = new Swiper(".championship-slider .swiper", {
+    loop: true,
+    autoplay: true,
+    preloadImages: false,
+    pagination: {
+      el: ".championship-slider .swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".championship-slider .swiper-btn-next",
+      prevEl: ".championship-slider .swiper-btn-prev",
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        spaceBetween: 15,
+      },
+      767: {
+        slidesPerView: 2,
+        spaceBetween: 15,
+      },
+      992: {
+        slidesPerView: 3,
+        spaceBetween: 15,
+      },
+      1199: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+    },
+  });
+});
+//showPass
+function showPass(showPass) {
+  sibling = showPass.parentElement.nextElementSibling;
+  // sibling.focus();
+  if (showPass.checked) {
+    sibling.setAttribute("type", "text");
+  } else {
+    sibling.setAttribute("type", "password");
+  }
+}
