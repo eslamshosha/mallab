92a45e8f83e9bf2a6fb9e67157c2aaaa5e2dc2b1
eslam shosha <you@example.comeslamshosha22.es@gmail.com>
2024-06-07 04:51:42 +0300

    store page

--- js/script.js
+++ js/script.js
@@ -135,6 +135,59 @@ $(document).ready(function () {
         otp_fields.eq(index).val(value);
       });
     });
+  ///////// ** offer** /////////
+  var specials = new Swiper(".offer-slider .swiper", {
+    loop: true,
+    autoplay: true,
+    autoHeight: true,
+    slidesPerView: 1,
+    pagination: {
+      el: ".offer-slider .swiper-pagination",
+      clickable: true,
+    },
+  });
+  ///////// **category-section** /////////
+  var categorySlider = new Swiper(".category-section .swiper", {
+    loop: true,
+    autoplay: true,
+    pagination: {
+      el: ".category-slider .swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".category-slider .swiper-btn-next",
+      prevEl: ".category-slider .swiper-btn-prev",
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 3,
+        spaceBetween: 15,
+      },
+      767: {
+        slidesPerView: 3,
+        spaceBetween: 15,
+      },
+      992: {
+        slidesPerView: 4,
+        spaceBetween: 30,
+      },
+      1199: {
+        slidesPerView: 8,
+        spaceBetween: 30,
+      },
+    },
+  });
+  //bannerSlider
+  var bannerSlider = new Swiper(".banner-section .swiper", {
+    loop: true,
+    autoplay: true,
+    autoHeight: true,
+    slidesPerView: 1,
+    pagination: {
+      el: ".banner-slider .swiper-pagination",
+      clickable: true,
+    },
+  });
 });
 //showPass
 function showPass(showPass) {
